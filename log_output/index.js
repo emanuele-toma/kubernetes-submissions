b6ddd7f5e0b7bd3fe899d4c019f730d24c0d97e6
Emanuele Toma <toma.emanuele03@gmail.com>
2025-08-20 22:56:23 +0200

    Exercise 1.7

--- log_output/index.js
+++ log_output/index.js
@@ -1,6 +1,18 @@
+const express = require('express');
 const random = btoa(Math.floor(Math.random() * 1_000_000_000));
 
 setInterval(() => {
   const date = new Date();
   console.log(`${date.toISOString()}: ${random}`);
 }, 5000);
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const date = new Date();
+  return res.send(`${date.toISOString()}: ${random}`);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
