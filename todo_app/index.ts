5f5d7a049b9a22e344f88f8400fcc8f440fa56fe
Emanuele Toma <toma.emanuele03@gmail.com>
2025-08-20 20:22:07 +0200

    Exercise 1.5

--- todo_app/index.ts
+++ todo_app/index.ts
@@ -1,11 +1,10 @@
 import { Hono } from 'hono';
+import { serveStatic } from 'hono/bun';
 
 const app = new Hono();
 const port = import.meta.env.PORT ?? 3000;
 
-app.get('/', c => {
-  return c.text('Hello, Hono!');
-});
+app.use('*', serveStatic({ root: './static' }));
 
 export default {
   port: port,
