0da1072024dd4fbb1238324fb1f92e349c526d73
Emanuele Toma <toma.emanuele03@gmail.com>
2025-08-22 22:29:27 +0200

    Exercise 1.10

--- log-output/index.js
+++ log-output/index.js
@@ -1,18 +1,27 @@
 const express = require('express');
+const fs = require('fs');
 const random = btoa(Math.floor(Math.random() * 1_000_000_000));
+
+const isWriter = process.env.WRITER === 'true';
 
 setInterval(() => {
   const date = new Date();
   console.log(`${date.toISOString()}: ${random}`);
+
+  if (isWriter) {
+    fs.writeFileSync('/data/log.txt', `${date.toISOString()}: ${random}`);
+  }
 }, 5000);
 
 const app = express();
 
 app.get('/', (req, res) => {
-  const date = new Date();
-  return res.send(`${date.toISOString()}: ${random}`);
+  const data = fs.readFileSync('/data/log.txt', 'utf8');
+  res.send(data);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}`);
-});
+if (!isWriter) {
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+  });
+}
