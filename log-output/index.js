dd19b5c884486ccaad0102943c3ae79f957e639c
Emanuele Toma <toma.emanuele03@gmail.com>
2025-08-22 23:58:09 +0200

    Exercise 1.11

--- log-output/index.js
+++ log-output/index.js
@@ -2,26 +2,22 @@ const express = require('express');
 const fs = require('fs');
 const random = btoa(Math.floor(Math.random() * 1_000_000_000));
 
-const isWriter = process.env.WRITER === 'true';
-
 setInterval(() => {
   const date = new Date();
   console.log(`${date.toISOString()}: ${random}`);
-
-  if (isWriter) {
-    fs.writeFileSync('/data/log.txt', `${date.toISOString()}: ${random}`);
-  }
 }, 5000);
 
 const app = express();
 
 app.get('/', (req, res) => {
-  const data = fs.readFileSync('/data/log.txt', 'utf8');
-  res.send(data);
+  const date = new Date();
+  if (!fs.existsSync('/data/pingpong.txt')) {
+    return res.send(`${date.toISOString()}: ${random}.\nPing / Pongs: 0`);
+  }
+  const data = fs.readFileSync('/data/pingpong.txt');
+  res.send(`${date.toISOString()}: ${random}.\nPing / Pongs: ${data}`);
 });
 
-if (!isWriter) {
-  app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 3000}`);
-  });
-}
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
--- ping-pong/index.ts
+++ ping-pong/index.ts
@@ -1,10 +1,15 @@
 import { Hono } from 'hono';
+import fs from 'fs';
 
 let counter = 0;
 
 const app = new Hono();
 
-app.get('/pingpong', c => c.text(`Pong ${counter++}`));
+app.get('/pingpong', c => {
+  counter++;
+  fs.writeFileSync('/data/pingpong.txt', counter.toString());
+  return c.text(`Pong ${counter}`);
+});
 
 export default {
   port: import.meta.env.PORT || 3000,
